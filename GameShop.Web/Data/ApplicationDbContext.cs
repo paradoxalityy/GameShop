6f05c56c2603d05ef72dbae59ecc24d3e5ce4bcd
paradoxalityy <szczepanik.szymek@gmail.com>
2023-03-18 04:15:38 +0100

    Created class for database context, added DbSet of game categories, also added DbContext into services in Program.cs file

---
+++ GameShop.Web/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using GameShop.Web.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GameShop.Web.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}

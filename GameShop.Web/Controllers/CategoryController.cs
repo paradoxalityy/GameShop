fbaeaed4951491db3f90a3d04c14c541e4ae7141
paradoxalityy <szczepanik.szymek@gmail.com>
2023-03-19 17:21:09 +0100

    Created InitialMigration and also updated the database. Created Index View for Category controller to test do all works fine. Also Created CategoryController with Index method that returns List of existing categories of games from DB

---
+++ GameShop.Web/Controllers/CategoryController.cs
@@ -0,0 +1,21 @@
+﻿using GameShop.Web.Data;
+using GameShop.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GameShop.Web.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Category> categories = _db.Categories.ToList();
+            return View(categories);
+        }
+    }
+}

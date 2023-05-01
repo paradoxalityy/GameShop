7d6d6fae787f7f55d868a435dada382f2b4534f5
paradoxality <szczepanik.szymek@gmail.com>
2023-05-01 18:18:44 +0200

    Created generic Repository class that implements IRepository interface

---
+++ GameShop.Data/Repository/Repository.cs
@@ -0,0 +1,51 @@
+﻿using GameShop.Data.Repository.IRepository;
+using GameShop.DataAccess.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameShop.Data.Repository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private ApplicationDbContext _db;
+        internal DbSet<T> _dbSet;
+        public Repository(ApplicationDbContext db)
+        {
+            _db = db;
+            _dbSet = _db.Set<T>();
+        }   
+
+        public T GetFirstOrDefault(Expression<Func<T, bool>> filter)
+        {
+            IQueryable<T> query = _dbSet;
+            query = query.Where(filter);
+            return query.FirstOrDefault();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            IQueryable<T> query = _dbSet;
+            return query.ToList();
+        }
+
+        public void Add(T entity)
+        {
+            _dbSet.Add(entity);
+        }
+
+        public void Remove(T entity)
+        {
+            _dbSet.Remove(entity);
+        }
+
+        public void RemoveRange(IEnumerable<T> entities)
+        {
+            _dbSet.RemoveRange(entities);
+        }
+    }
+}

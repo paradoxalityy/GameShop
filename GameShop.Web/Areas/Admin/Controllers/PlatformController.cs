13f22b231d23bb506110ac7fff8ad3fd96f546c8
paradoxalityy <szczepanik.szymek@gmail.com>
2023-05-02 21:59:44 +0200

    - Added views for deleting and editing platform. - Also created post and get methods for it. - Fixed partial view misspelling in _Layout view file

--- GameShop.Web/Areas/Admin/Controllers/PlatformController.cs
+++ GameShop.Web/Areas/Admin/Controllers/PlatformController.cs
@@ -26,6 +26,7 @@ namespace GameShop.Web.Areas.Admin.Controllers
         }
 
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public IActionResult Create(Platform platform)
         {
             if (ModelState.IsValid)
@@ -37,6 +38,77 @@ namespace GameShop.Web.Areas.Admin.Controllers
             }
 
             return View(platform);
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var category = _unitOfWork.Platform.GetFirstOrDefault(p => p.Id == id);
+            if(category == null)
+            {
+                return NotFound();
+            }
+
+            return View(category);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Platform platform)
+        {
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.Platform.Update(platform);
+                _unitOfWork.Save();
+                TempData["Success"] = "Platform Edited Successfully.";
+                return RedirectToAction("Index");
+            }
+
+            return View(platform);
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int? id)
+        {
+            if(id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var platformToDelete = _unitOfWork.Platform.GetFirstOrDefault(p => p.Id == id);
+            if(platformToDelete == null)
+            {
+                return NotFound();
+            }
+
+            return View(platformToDelete);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePOST(int? id)
+        {
+            if(id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var platformToDelete = _unitOfWork.Platform.GetFirstOrDefault(p => p.Id == id);
+            if(platformToDelete == null)
+            {
+                return NotFound();
+            }
+
+            _unitOfWork.Platform.Remove(platformToDelete);
+            _unitOfWork.Save();
+
+            TempData["Success"] = "Platform Deleted Successfully.";
+            return RedirectToAction("Index");
         }
     }
 }

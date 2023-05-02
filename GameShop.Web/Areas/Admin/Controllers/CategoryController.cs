73ca452d39062fc026f34258d48bdb0c173db1bc
paradoxalityy <szczepanik.szymek@gmail.com>
2023-05-02 03:38:24 +0200

    Added PlatformController and Index view for it

--- GameShop.Web/Areas/Admin/Controllers/CategoryController.cs
+++ GameShop.Web/Areas/Admin/Controllers/CategoryController.cs
@@ -6,6 +6,7 @@ using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 namespace GameShop.Web.Areas.Admin.Controllers
 {
+    [Area("Admin")]
     public class CategoryController : Controller
     {
         private readonly IUnitOfWork _unitOfWork;
---
+++ GameShop.Web/Areas/Admin/Controllers/PlatformController.cs
@@ -0,0 +1,22 @@
+﻿using GameShop.Data.Repository.IRepository;
+using GameShop.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GameShop.Web.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class PlatformController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public PlatformController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Platform> platforms = _unitOfWork.Platform.GetAll();
+            return View(platforms);
+        }
+    }
+}

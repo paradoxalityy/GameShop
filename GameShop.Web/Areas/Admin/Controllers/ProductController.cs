3f556e2fecc0071d242d872dc201ffe80444428e
paradoxalityy <szczepanik.szymek@gmail.com>
2023-05-03 17:45:41 +0200

    Added passing list of products and platforms to the view by using two options viewbag and viewdata

--- GameShop.Web/Areas/Admin/Controllers/ProductController.cs
+++ GameShop.Web/Areas/Admin/Controllers/ProductController.cs
@@ -1,6 +1,7 @@
 ﻿using GameShop.Data.Repository.IRepository;
 using GameShop.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace GameShop.Web.Areas.Admin.Controllers
 {
@@ -23,9 +24,25 @@ namespace GameShop.Web.Areas.Admin.Controllers
         public IActionResult Upsert(int? id)
         {
             Product product = new Product();
+
+            IEnumerable<SelectListItem> productCategories = _unitOfWork.Category.GetAll().Select(
+                c => new SelectListItem
+                {
+                    Text = c.Name,
+                    Value = c.Id.ToString()
+                });
+            IEnumerable<SelectListItem> productPlatforms = _unitOfWork.Platform.GetAll().Select(
+                p => new SelectListItem
+                {
+                    Text = p.Name,
+                    Value = p.Id.ToString()
+                });
+
             if(id == null || id == 0)
             {
-                // Creating new product
+                // Using two ways of passing data to the view
+                ViewBag.productCategories = productCategories;
+                ViewData["productPlatforms"] = productPlatforms;
                 return View(product);
             }
             else

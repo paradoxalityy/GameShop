5eba3935230d0922f0f91b6e36455b576cfa9b74
paradoxalityy <szczepanik.szymek@gmail.com>
2023-05-03 17:14:22 +0200

    Started working on Upsert method for Product controller. Created index view for Product controller that will display list of all avaiable products

---
+++ GameShop.Web/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,39 @@
+﻿using GameShop.Data.Repository.IRepository;
+using GameShop.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GameShop.Web.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public ProductController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Product> products = _unitOfWork.Product.GetAll();
+            return View(products);
+        }
+
+        [HttpGet]
+        public IActionResult Upsert(int? id)
+        {
+            Product product = new Product();
+            if(id == null || id == 0)
+            {
+                // Creating new product
+                return View(product);
+            }
+            else
+            {
+                // Updating existing product
+            }
+
+            return View(product);
+        }
+    }
+}

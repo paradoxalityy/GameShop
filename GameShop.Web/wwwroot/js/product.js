f8291e2c4a39b6f7fdc064c76e49281deb16a8d0
paradoxalityy <szczepanik.szymek@gmail.com>
2023-05-06 03:28:22 +0200

    Added one more column in productsDataTable, it renders two buttons: one for editing products data and one for deleting product

--- GameShop.Web/wwwroot/js/product.js
+++ GameShop.Web/wwwroot/js/product.js
@@ -15,7 +15,22 @@ function loadProductsDataTable() {
             { "data": "publisher", "width": "15%" },
             { "data": "category.name", "width": "15%" },
             { "data": "platform.name", "width": "15%" },
-            { "data": "price", "width": "15%" }
+            { "data": "price", "width": "15%" },
+            {
+                "data": "id",
+                "render": function (data) {
+                    return `<div class="w-75 btn-group role="group">
+                                <a href="/Admin/Product/Upsert?id=${data}" class="btn btn-outline-primary mx-4">
+                                    <i class="bi bi-pencil-square"></i> &nbsp; Edit
+                                </a>
+                                <a class="btn btn-outline-danger mx-4">
+                                    <i class="bi bi-trash3"></i> &nbsp; Delete
+                                </a>
+                            </div>
+                    `
+                },
+                "width":"15%"
+            }
         ]
     });
 }

d1bb0cbef69a0d018e1d4b3518dfe4d1b0ede66a
paradoxalityy <szczepanik.szymek@gmail.com>
2023-05-07 21:09:15 +0200

    Main page of application now displays all avaiable products

--- GameShop.Web/Areas/Customer/Controllers/HomeController.cs
+++ GameShop.Web/Areas/Customer/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using GameShop.Models;
+﻿using GameShop.Data.Repository.IRepository;
+using GameShop.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -8,15 +9,18 @@ namespace GameShop.Web.Areas.Customer.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IUnitOfWork _unitOfWork;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
         {
             _logger = logger;
+            _unitOfWork = unitOfWork;
         }
 
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Product> avaiableProducts = _unitOfWork.Product.GetAll(includeProperties: "Category,Platform");
+            return View(avaiableProducts);
         }
 
         public IActionResult Privacy()

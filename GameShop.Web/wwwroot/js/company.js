925d533e5779c092c75372b89a8b071a241089be
paradoxalityy <szczepanik.szymek@gmail.com>
2023-05-14 05:06:34 +0200

    Fixed company.js script. Company index view displays data in a proper way

--- GameShop.Web/wwwroot/js/company.js
+++ GameShop.Web/wwwroot/js/company.js
@@ -11,25 +11,25 @@ function loadCompaniesDataTable() {
         },
         "columns": [
             { "data": "name", "width": "15%" },
-            { "data": "streetAddress", "width": "15%" },
-            { "data": "city", "width": "15%" },
-            { "data": "state", "width": "15%" },
-            { "data": "postalCode", "width": "15%" },
-            { "data": "phoneNumber", "width": "15%" },
+            { "data": "streetAddress", "width": "10%" },
+            { "data": "city", "width": "10%" },
+            { "data": "state", "width": "10%" },
+            { "data": "postalCode", "width": "10%" },
+            { "data": "phoneNumber", "width": "10%" },
             {
                 "data": "id",
-                "render": function () {
-                    `<div class="w-75 btn group" role="group">
-                        <a href="/Admin/Company/Upsert?id=${data}" class="btn btn-outline-primary mx-4">
-                            <i class="bi bi-pencil-square"></i> &nbsp; Edit
-                        </a>
-                        <a onclick=Delete('/Admin/Product/Delete/${data}') class="btn btn-outline-danger mx-4">
-                            <i class="bi bi-trash3"></i> &nbsp; Delete
-                        </a>
-                    </div>
-                    `
+                "render": function (data) {
+                     return `<div class="w-100 btn group" role="group">
+                                <a href="/Admin/Company/Upsert?id=${data}" class="btn btn-outline-primary mx-2">
+                                    <i class="bi bi-pencil-square"></i> &nbsp; Edit
+                                </a>
+                                <a onclick=Delete('/Admin/Product/Delete/${data}') class="btn btn-outline-danger mx-2">
+                                    <i class="bi bi-trash3"></i> &nbsp; Delete
+                                </a>
+                            </div>
+                     `
                 },
-                "width": "15%"
+                "width": "20%"
             }
         ]
     });

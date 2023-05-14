c10ee1790a043a8c6a3a6481aed5a81366a93363
paradoxalityy <szczepanik.szymek@gmail.com>
2023-05-14 04:38:30 +0200

    Added displayname attribute for some props in Company model

--- GameShop.Models/Company.cs
+++ GameShop.Models/Company.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
@@ -13,10 +14,13 @@ namespace GameShop.Models
         public int Id { get; set; }
         [Required]
         public string Name { get; set; }
-        public string StreetAddress { get; set; }
-        public string City { get; set; }
-        public string State { get; set; }
-        public string PostalCode { get; set; }
-        public string PhoneNumber { get; set; }
+        [DisplayName("Street Address")]
+        public string? StreetAddress { get; set; }
+        public string? City { get; set; }
+        public string? State { get; set; }
+        [DisplayName("Postal Code")]
+        public string? PostalCode { get; set; }
+        [DisplayName("Phone Number")]
+        public string? PhoneNumber { get; set; }
     }
 }

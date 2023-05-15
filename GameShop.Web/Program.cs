8d973ba091c6da21e79abe9abf208735b6321f83
paradoxalityy <szczepanik.szymek@gmail.com>
2023-05-15 03:42:58 +0200

    Added EmailSender class that fixes problem with injecting it into Register razor page. This class later will get logic that will make it able to send emails.

--- GameShop.Web/Program.cs
+++ GameShop.Web/Program.cs
@@ -3,6 +3,8 @@ using GameShop.Data.Repository.IRepository;
 using GameShop.DataAccess.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using GameShop.Utility;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,10 +14,10 @@ builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(
     builder.Configuration.GetConnectionString("DefaultConnection")));
 
-builder.Services.AddDefaultIdentity<IdentityUser>().
-                 AddEntityFrameworkStores<ApplicationDbContext>();
-
+builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddDefaultTokenProviders().
+    AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+builder.Services.AddSingleton<IEmailSender, EmailSender>();
 
 var app = builder.Build();
 

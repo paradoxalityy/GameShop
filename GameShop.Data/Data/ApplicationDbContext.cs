93f454a02800fe62b4f32b1be937b30088367fc1
paradoxality <szczepanik.szymek@gmail.com>
2023-07-09 21:40:35 +0200

    Added order header and order details models. Registered them as DbSet in DbContext class. Added new migration into database.

--- GameShop.Data/Data/ApplicationDbContext.cs
+++ GameShop.Data/Data/ApplicationDbContext.cs
@@ -18,5 +18,7 @@ namespace GameShop.DataAccess.Data
         public DbSet<Company> Companies { get; set; }
         public DbSet<ApplicationUser> ApplicationUsers { get; set; }
         public DbSet<ShoppingCart> ShoppingCarts { get; set; }
+        public DbSet<OrderHeader> OrderHeaders { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
     }
 }

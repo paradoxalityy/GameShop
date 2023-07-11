4b321930a644d55fa1a6aa135d16a1386d745ef6
paradoxalityy <szczepanik.szymek@gmail.com>
2023-07-11 15:56:03 +0200

    Added new static details (order and payment status types)

--- GameShop.Utility/SD.cs
+++ GameShop.Utility/SD.cs
@@ -9,9 +9,25 @@ namespace GameShop.Utility
     // SD ~ Static Details
     public static class SD
     {
+        // Application roles
         public const string Role_User_Indi = "Individual";
         public const string Role_User_Comp = "Company";
         public const string Role_User_Admin = "Admin";
         public const string Role_User_Employee = "Employee";
+
+        // Order Status
+        public const string OrderStatusPending = "Pending";
+        public const string OrderStatusApproved = "Approved";
+        public const string OrderStatusInProcess = "Processing";
+        public const string OrderStatusShipped = "Shipped";
+        public const string OrderStatusCancelled = "Cancelled";
+        public const string OrderStatusRefunded = "Refunded";
+
+
+        // Payment Status
+        public const string PaymentStatusPending = "Pending";
+        public const string PaymentStatusApproved = "Approved";
+        public const string PaymentStatusDelayedPayment = "ApprovedForDelayedPayment";
+        public const string PaymentStatusRejected = "Rejected";
     }
 }

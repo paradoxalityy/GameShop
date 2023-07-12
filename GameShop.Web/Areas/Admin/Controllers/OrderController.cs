88ce4525b37c3236e917df4fd16be4cfa9f0c4bb
paradoxalityy <szczepanik.szymek@gmail.com>
2023-07-12 15:45:26 +0200

    Added API endpoint that returns JSON with informations about all placed orders.

---
+++ GameShop.Web/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,29 @@
+﻿using GameShop.Data.Repository.IRepository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GameShop.Web.Areas.Admin.Controllers
+{
+	public class OrderController : Controller
+	{
+		private readonly IUnitOfWork _unitOfWork;
+		public OrderController(IUnitOfWork unitOfWork)
+		{
+			_unitOfWork = unitOfWork;
+		}
+
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		#region API CALLS
+
+		public IActionResult GetAll()
+		{
+			var orderHeaders = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser");
+			return Json(new { data = orderHeaders });
+		}
+
+		#endregion API CALLS
+	}
+}
